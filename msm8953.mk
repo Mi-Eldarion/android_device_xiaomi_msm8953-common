07520172a15c529778d1e0b13f5ea6671d77e82f
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-02-05 17:37:32 +0700

    msm8953-common: msm8953 target QSSI

--- msm8953.mk
+++ msm8953.mk
@@ -25,6 +25,9 @@ PRODUCT_AAPT_PREF_CONFIG ?= xxhdpi
 TARGET_SCREEN_HEIGHT ?= 1920
 TARGET_SCREEN_WIDTH ?= 1080
 
+# Platform
+TARGET_BOARD_PLATFORM ?= msm8953
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
